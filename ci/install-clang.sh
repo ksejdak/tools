cd58876e1aedebef813f1b60a97cc74752e9f347
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-07-29 16:13:13 +0200

    Use clang short version when exporting CC.

--- ci/install-clang.sh
+++ ci/install-clang.sh
@@ -27,7 +27,6 @@ fi
 echo "Installing clang v${VERSION}"
 
 if [ "${OS}" == "linux" ]; then
-    SHORT_VERSION=`echo ${VERSION} | cut -d . -f 1-2`
     PACKAGE_NAME="clang+llvm-${VERSION}-x86_64-linux-gnu-ubuntu-14.04"
     PACKAGE_BIN_NAME="${PACKAGE_NAME}.tar.xz"
     PACKAGE_URL="http://releases.llvm.org/${VERSION}/${PACKAGE_BIN_NAME}"
@@ -49,8 +48,9 @@ else
     echo "/usr/local/opt/llvm/bin" >> ~/path_exports
 fi
 
+SHORT_VERSION=`echo ${VERSION} | cut -d . -f 1-2`
 if [ "${EXPORT}" == "true" ]; then
-    echo "export CC=clang-${VERSION}" >> ~/.bash_profile
+    echo "export CC=clang-${SHORT_VERSION}" >> ~/.bash_profile
     echo "export CXX=clang++" >> ~/.bash_profile
 fi
 
