454b689ca533a1c32307ea4d55f4665048b2f74d
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-08-21 22:08:10 +0200

    Break cmake config if dependant packages are not found.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -2,19 +2,25 @@ function(utils_log MSG)
     message(STATUS "  ${MSG}")
 endfunction()
 
+function(utils_error MSG)
+    message(FATAL_ERROR "  ${MSG}")
+endfunction()
+
 macro(utils_getVersion VERSION)
     find_package(Git)
-    if(GIT_FOUND)
-        execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags OUTPUT_VARIABLE TAG_VERSION ERROR_VARIABLE TAG_VERSION)
-        string(STRIP ${TAG_VERSION} TAG_VERSION)
-        string(SUBSTRING ${TAG_VERSION} 1 -1 TAG_VERSION)
-
-        if(TAG_VERSION MATCHES "^(fatal)")
-            set(TAG_VERSION "0.1.0")
-        endif()
-
-        set(VERSION ${TAG_VERSION})
+    if (NOT GIT_FOUND)
+        utils_error("Git not found")
     endif()
+
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags OUTPUT_VARIABLE TAG_VERSION ERROR_VARIABLE TAG_VERSION)
+    string(STRIP ${TAG_VERSION} TAG_VERSION)
+    string(SUBSTRING ${TAG_VERSION} 1 -1 TAG_VERSION)
+
+    if(TAG_VERSION MATCHES "^(fatal)")
+        set(TAG_VERSION "0.1.0")
+    endif()
+
+    set(VERSION ${TAG_VERSION})
 endmacro()
 
 function(utils_setOutputPath TARGET OUTPUT_PATH)
