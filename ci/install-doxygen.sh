008ad2cfc719c29dacf0cb9e37508362772f45c0
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-08-23 11:05:12 +0200

    Install doxygen by hand.

--- ci/install-doxygen.sh
+++ ci/install-doxygen.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
-# $1 - Host OS (Linux or macOS)
+# $1 - doxygen version to be installed (e.g. 3.8.2)
+# $2 - Host OS (Linux or macOS)
 
 set -ev
 
-OS=${1}
+VERSION=${1}
+OS=${2}
 
-if [ -z ${OS} ]; then
-    echo "No host OS specified. Aborting."
+if [ -z ${VERSION} ]; then
+    echo "No doxygen version specified. Aborting."
     exit 1
 fi
 
-echo "Installing doxygen"
-
-if [ "${OS}" == "linux" ]; then
-    sudo apt-get install doxygen -y
-else
-    brew install doxygen
+if [ -z ${OS} ]; then
+    echo "No host OS specified. Aborting."
+    exit 2
 fi
 
-echo "Installing doxygen OK."
+if [ -d doxygen ]; then
+    exit 0
+fi
+
+echo "Installing doxygen v${VERSION}."
+
+PACKAGE_NAME="doxygen-${VERSION}"
+if [ "${OS}" == "linux" ]; then
+    PACKAGE_BIN_NAME="${PACKAGE_NAME}.linux.bin.tar.gz"
+else
+    PACKAGE_BIN_NAME="${PACKAGE_NAME}.dmg"
+fi
+PACKAGE_URL="ftp://ftp.stack.nl/pub/users/dimitri/${PACKAGE_BIN_NAME}"
+
+wget --no-check-certificate --quiet ${PACKAGE_URL}
+if [ ! -f ${PACKAGE_BIN_NAME} ]; then
+    echo "Failed to download doxygen v${VERSION}."
+    exit 3
+fi
+
+mkdir -p doxygen
+
+if [ "${OS}" == "linux" ]; then
+    tar --strip-components=1 -xf ${PACKAGE_BIN_NAME} -C doxygen
+    echo "${PWD}/doxygen/bin" >> ~/path_exports
+else
+    hdiutil attach ${PACKAGE_BIN_NAME}
+    cp -R /Volumes/Doxygen/* doxygen/
+    hdiutil unmount /Volumes/Doxygen
+    echo "${PWD}/doxygen/Doxygen.app/Contents/Resources" >> ~/path_exports
+fi
+
+echo "Installing doxygen v${VERSION} OK."
