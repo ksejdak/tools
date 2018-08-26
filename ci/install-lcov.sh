083de04ab8c93723b51215ca4b1045df712767dc
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-08-26 17:12:38 +0200

    Add script for installing lcov.

---
+++ ci/install-lcov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# $1 - Host OS (Linux or macOS)
+
+set -ev
+
+OS=${1}
+
+if [ -z ${OS} ]; then
+    echo "No host OS specified. Aborting."
+    exit 1
+fi
+
+echo "Installing lcov."
+
+if [ "${OS}" == "linux" ]; then
+    sudo apt-get install lcov -y
+else
+    brew install lcov
+fi
+echo "Installing lcov OK."
