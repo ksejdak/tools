d96e1ad9796188ea01ad6cffbfc20903d8f4ea61
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-09-23 19:45:07 +0200

    Add proper cppcheck error handling.

--- ci/check-cppcheck.sh
+++ ci/check-cppcheck.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 # $1 - path to includes
 # $2 - path to sources
+# $3 - path to reference cppcheck.warn file
 
-set -e
+INCLUDE_PATH=${1}
+SRC_PATH=${2}
+REFERENCE_PATH=${3}
 
-if [ -z ${1} ]; then
+if [ -z ${INCLUDE_PATH} ]; then
     echo "No include path specified. Aborting."
     exit 1
 fi
 
-if [ -z ${2} ]; then
+if [ -z ${SRC_PATH} ]; then
     echo "No source path specified. Aborting."
     exit 2
 fi
 
-OUTPUT=`tools/code/check-cppcheck.sh ${1} ${2}`
-INVALID_LINES=`echo "${OUTPUT}" | wc -l | xargs`
-if [ ${INVALID_LINES} -ne 0 ]; then
-    echo "cppcheck.sh found ${INVALID_LINES} issues:"
-    echo ${OUTPUT}
+if [ -z ${REFERENCE_PATH} ]; then
+    echo "No reference cppcheck.warn path specified. Aborting."
     exit 3
+fi
+
+OUTPUT=`tools/code/check-cppcheck.sh ${INCLUDE_PATH} ${SRC_PATH}`
+
+if [ -f ${REFERENCE_PATH} ]; then
+    while IFS= read -r LINE; do
+        RESULT=`grep -F "${LINE}" "${REFERENCE_PATH}"`
+        if [ -z "${RESULT}" ]; then
+            echo "cppcheck error: '${OUTPUT}'"
+            exit 4
+        fi
+    done < <(printf '%s\n' "${OUTPUT}")
 fi
 
 echo "cppcheck check OK."
--- code/check-cppcheck.sh
+++ code/check-cppcheck.sh
@@ -4,14 +4,17 @@
 
 set -e
 
-if [ -z ${1} ]; then
+INCLUDE_PATH=${1}
+SRC_PATH=${2}
+
+if [ -z ${INCLUDE_PATH} ]; then
     echo "No include path specified. Aborting."
     exit 1
 fi
 
-if [ -z ${2} ]; then
+if [ -z ${SRC_PATH} ]; then
     echo "No source path specified. Aborting."
     exit 2
 fi
 
-cppcheck --quiet -I ${1} --language=c++ --enable=warning,performance,portability,missingInclude ${2} 2>&1
+cppcheck --quiet -I ${INCLUDE_PATH} --language=c++ --enable=warning,performance,portability,missingInclude ${SRC_PATH} 2>&1
