93ae6dd5f39443dad1edd1ff00564c9dd4c81e04
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-09-24 15:58:49 +0200

    Add test clang-tidy checks.

--- code/check-clang-tidy.sh
+++ code/check-clang-tidy.sh
@@ -7,5 +7,24 @@ if [ ! -f "compile_commands.json" ]; then
     exit 1
 fi
 
-echo "++++++++++++++"
-python ../tools/code/run-clang-tidy.py -checks=*
+python3 ../tools/code/run-clang-tidy.py -quiet -export-fixes='clang-tidy.warn' -checks='*,
+                                                                                       -cppcoreguidelines-pro-type-union-access,
+                                                                                       -cppcoreguidelines-pro-type-member-init,
+                                                                                       -cppcoreguidelines-pro-bounds-array-to-pointer-decay,
+                                                                                       -cppcoreguidelines-pro-bounds-constant-array-index,
+                                                                                       -cppcoreguidelines-pro-type-reinterpret-cast,
+                                                                                       -cppcoreguidelines-pro-bounds-pointer-arithmetic,
+                                                                                       -google-readability-braces-around-statements,
+                                                                                       -google-readability-function-size,
+                                                                                       -google-build-using-namespace,
+                                                                                       -hicpp-braces-around-statements,
+                                                                                       -hicpp-no-array-decay,
+                                                                                       -hicpp-member-init,
+                                                                                       -hicpp-function-size,
+                                                                                       -readability-implicit-bool-conversion,
+                                                                                       -readability-braces-around-statements,
+                                                                                       -readability-redundant-member-init,
+                                                                                       -readability-function-size,
+                                                                                       -modernize-use-default-member-init,
+                                                                                       -fuchsia-statically-constructed-objects' >/dev/null 2>&1
+                                                                                       #-fuchsia-default-arguments' >/dev/null 2>&1
