493d3b21f9d6377695a4d214c5c144d2d4ae248c
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-09-24 13:15:39 +0200

    Add scripts for clang-tidy checks.

---
+++ ci/check-clang-tidy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# $1 - build path
+
+BUILD_PATH=${1}
+
+if [ -z ${BUILD_PATH} ]; then
+    echo "No build path specified. Aborting."
+    exit 1
+fi
+
+cd ${BUILD_PATH}
+
+../tools/code/check-clang-tidy.sh
+CLANG_CHECK_ERR=${?}
+
+cd -
+
+if [ ${CLANG_CHECK_ERR} -ne 0 ]; then
+    echo "Failed to run check-clang-tidy.sh script."
+    exit 2
+fi
+
+echo "clang-tidy check OK."
---
+++ code/check-clang-tidy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f "compile_commands.json" ]; then
+    echo "No 'compile_commands.json' file found. Aborting."
+    exit 1
+fi
+
+python ../tools/code/run-clang-tidy.py -checks=*
