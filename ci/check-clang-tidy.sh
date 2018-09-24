48e897d56625af8de736e6160b1a7c0e9c5922fe
Kuba Sejdak <kuba.sejdak@gmail.com>
2018-09-24 16:27:17 +0200

    Load clang-tidy config from file.

--- ci/check-clang-tidy.sh
+++ ci/check-clang-tidy.sh
@@ -10,25 +10,14 @@ fi
 
 cd ${BUILD_PATH}
 
-GENERATED_WARN_PATH="clang-tidy.warn"
-if [ -f ${GENERATED_WARN_PATH} ]; then
-    rm ${GENERATED_WARN_PATH}
-fi
-
 ../tools/code/check-clang-tidy.sh
 CLANG_CHECK_ERR=$?
 
 cd -
 
 if [ ${CLANG_CHECK_ERR} -ne 0 ]; then
-    echo "Failed to run check-clang-tidy.sh script."
+    echo "Clang-tidy found some errors."
     exit 2
-fi
-
-if [ -s ${BUILD_PATH}/${GENERATED_WARN_PATH} ]; then
-    echo "Clang-tidy found some issues:"
-    cat ${BUILD_PATH}/${GENERATED_WARN_PATH}
-    exit 3
 fi
 
 echo "Clang-tidy check OK."
